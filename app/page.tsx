9776ed46538b0e18abd16ecb61ae64e1b2545563
Anna Boiko <annaboiko1@icloud.com>
2026-01-28 22:14:01 +0100

    feat: update Home component layout and styling, enhance hero section with image and text

--- app/page.tsx
+++ app/page.tsx
@@ -14,30 +14,35 @@ export default function Home() {
 
   return (
     <>
-      {/* Hero Section з фото + текст */}
-      <Container maxWidth="md" sx={{ py: 8, textAlign: 'center', mt: 4 }}>
-        {/* Твоє фото */}
-        <Box sx={{ mb: 4 }}>
-          <Image 
-            src="/annaboiko.png"  // ← Завантаж фото в public/anna-photo.jpg
-            alt="Anna Boiko"
-            width={300}
-            height={300}
-            className="object-cover shadow-2xl"
-            priority
-          />
-        </Box>
-        
-        {/* Текст з скріншота */}
-        <Typography variant="h1" sx={{ fontSize: { xs: '2.5rem', md: '3.5rem' }, fontWeight: 700, mb: 2, lineHeight: 1.1 }}>
-          Hi, my name is <span style={{ color: 'var(--blue)' }}>Anna</span>.
+      
+      <Container maxWidth="md" sx={{ py:8, mt: 4}}>
+
+        <Container maxWidth="md" sx={{ display:'flex', alignItems: 'flex-end'}}>
+          <Typography variant="h1" sx={{ fontSize: { xs: '3rem', md: '3.5rem' }, fontWeight: 500, mb: 2, lineHeight: 1.3 }}>
+            Hi, my 
+          </Typography>
+          <Box sx={{ mb: 4 }}>
+            <Image 
+              src="/annaboiko.png"  
+              alt="Anna Boiko"
+              width={300}
+              height={300}
+              className=""
+              priority
+            />
+          </Box>
+        </Container>
+      
+        <Typography variant="h1" sx={{ fontSize: { xs: '3rem', md: '3.5rem' }, fontWeight: 500, mb: 10, lineHeight: 0.5, px:2 }}>
+          name is <strong>Anna</strong><span style={{ color: 'var(--blue)' }}>.</span>
         </Typography>
-        <Typography variant="h5" sx={{ fontSize: { xs: '1.1rem', md: '1.25rem' }, color: 'text.secondary', mb: 6, maxWidth: 600, mx: 'auto' }}>
-          I'm an independent, creative full-stack web developer and designer from Ukraine, currently based in Germany.
+
+        <Typography variant="h3" sx={{ fontSize: { xs: '1.5rem', md: '1.25rem' }, color: 'text', mb: 2, maxWidth: 600,px:2 }}>
+          I&apos;m an independent, creative <strong>full-stack web developer</strong> and designer from Ukraine, based in Toronto.
         </Typography>
       </Container>
 
-      {/* Навбар знизу */}
+      {}
       <Box sx={{ width: '100%', bottom: 0, position: "fixed", left: 0, right: 0 }}>
         <BottomNavigation
           value={pathname}
