e61b93a4dcd3a9d4214d4ce7ca82c8eba476947c
Anna Boiko <annaboiko1@icloud.com>
2026-02-11 20:29:50 -0500

    style: Add responsive sizing and positioning to the Hire Me component.

--- components/HireMe.tsx
+++ components/HireMe.tsx
@@ -16,28 +16,28 @@ export default function HireMe() {
   };
 
   return (
-    <div className="fixed bottom-28 right-6 z-1000 flex items-center gap-1">
+    <div className="fixed bottom-28 right-6 md:right-20 z-1000 flex items-center gap-1">
       <button
         onClick={handleClick}
-        className="w-25 h-10 gap-1 bg-primary-blue hover:bg-primary-red text-white rounded-full  transition-all duration-300 flex items-center justify-center p-0"
-        title="Hire Me" 
-        
+        className="w-25 md:w-32 h-10 md:h-12 gap-1 bg-primary-blue hover:bg-primary-red text-white rounded-full transition-all duration-300 flex items-center justify-center p-0"
+        title="Hire Me"
+
       >
-      <span className="text-gray-800 font-semibold text-sm whitespace-nowrap center ">
-        Hire Me
-      </span>
-        <FontAwesomeIcon icon={faCommentDots}  size="2x" 
-        className="w-8 h-8 bg-(--blue) rounded-[45%] text-(--purple) shadow-md hover:bg-(--purple) hover:text-(--blue) transition-colors duration-300" />
+        <span className="text-gray-800 font-semibold text-sm md:text-lg whitespace-nowrap center ">
+          Hire Me
+        </span>
+        <FontAwesomeIcon icon={faCommentDots} size="2x"
+          className="w-8 h-8 md:w-10 md:h-10 bg-(--blue) rounded-[45%] text-(--purple) shadow-md hover:bg-(--purple) hover:text-(--blue) transition-colors duration-300" />
       </button>
 
       <button
         onClick={() => setIsVisible(false)}
         title="Close"
         style={{
-        color: 'var(--blue)',
-        transform: 'translate(-4px,-10px)'
-        
-      }}
+          color: 'var(--blue)',
+          transform: 'translate(-4px,-10px)'
+
+        }}
       >
         ×
       </button>
